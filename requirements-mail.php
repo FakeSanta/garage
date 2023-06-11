1b6253fa2442288fbb2bcf3354c87983078f9c21
friziiark <aurelienfevrier08@gmail.com>
2023-06-11 16:41:28 +0000

    del

--- requirements-mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-require '/var/www/vendor/phpmailer/phpmailer/src/Exception.php';
-require '/var/www/vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require '/var/www/vendor/phpmailer/phpmailer/src/SMTP.php';
-?>
