4879b003177bd0e470e2edb3c3889b9f930a8086
friziiark <aurelienfevrier08@gmail.com>
2023-06-11 10:35:21 +0000

    add UTF-8 to Mail

--- daily_check_ct.php
+++ daily_check_ct.php
@@ -27,6 +27,7 @@
         if($days_diff['diff_date'] < 360 && $days_diff['diff_date'] >= 1 && $row['rdv_pris'] == 0){
 
             $mail = new PHPMailer();
+            $mail->CharSet = "UTF-8";
             $mail->IsSMTP();
             $mail->Mailer = "smtp";
             $mail->SMTPDebug  = 0;  
@@ -47,13 +48,13 @@
                 $new_date = strftime("%d %B %Y",strtotime($row["prochaine_date_ct"]));
                 $new_date = str_replace( 
                     array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-                    array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'),
+                    array('Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'),
                     $new_date
                   );
                 $mail->SetFrom("supervision.decomble@gmail.com", "Auto ".$brend);
-                $mail->Subject = "J-".$days_diff['diff_date']." ! Controle technique a venir pour ".$row['marque']." ".$row['modele']." (".$row['immatriculation'].")";
-                $content = "J-".$days_diff['diff_date']." ! Controle technique a venir pour ".$row['marque']." ".$row['modele']." (".$row['immatriculation']." | date butoire le ".$new_date.")";
-                $contentDiscord = "**J-".$days_diff['diff_date']."** ! Controle technique a venir pour *".$row['marque']." ".$row['modele']."* (***".$row['immatriculation']."*** | date butoire le **".$new_date."**)";
+                $mail->Subject = "J-".$days_diff['diff_date']." ! Contrôle technique à venir pour ".$row['marque']." ".$row['modele']." (".$row['immatriculation'].")";
+                $content = "J-".$days_diff['diff_date']." ! Contrôle technique à venir pour ".$row['marque']." ".$row['modele']." (".$row['immatriculation']." | date butoire le ".$new_date.")";
+                $contentDiscord = "**J-".$days_diff['diff_date']."** ! Contrôle technique à venir pour *".$row['marque']." ".$row['modele']."* (***".$row['immatriculation']."*** | date butoire le **".$new_date."**)";
                 sendDiscordAlert($contentDiscord);
                 $mail->MsgHTML($content); 
                 if(!$mail->Send()) {
@@ -65,6 +66,7 @@
             }
         }elseif($days_diff['diff_date'] <= 0 && $row['rdv_pris'] == 0){
             $mail = new PHPMailer();
+            $mail->CharSet = "UTF-8";
             $mail->IsSMTP();
             $mail->Mailer = "smtp";
             $mail->SMTPDebug  = 0;  
@@ -85,13 +87,13 @@
                 $new_date = strftime("%d %B %Y",strtotime($row["prochaine_date_ct"]));
                 $new_date = str_replace( 
                     array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-                    array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'),
+                    array('Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'),
                     $new_date
                   );
                 $mail->SetFrom("supervision.decomble@gmail.com", "Auto ".$brend);
-                $mail->Subject = "!!!! Controle technique perime depuis ".abs($days_diff['diff_date'])." jours pour ".$row['modele']." ".$row['marque']." | ".$row['immatriculation'];
-                $content = "Controle technique perime depuis ".abs($days_diff['diff_date'])." jours pour ".$row['modele']." ".$row['marque']." | ".$row['immatriculation'];
-                $contentDiscord = "Controle technique perime depuis ***".abs($days_diff['diff_date'])." jours*** pour ".$row['modele']." ".$row['marque']." | ".$row['immatriculation'];
+                $mail->Subject = "!!!! Contrôle technique périmé depuis ".abs($days_diff['diff_date'])." jours pour ".$row['modele']." ".$row['marque']." | ".$row['immatriculation'];
+                $content = "Contrôle technique périmé depuis ".abs($days_diff['diff_date'])." jours pour ".$row['modele']." ".$row['marque']." | ".$row['immatriculation'];
+                $contentDiscord = "Contrôle technique périmé depuis ***".abs($days_diff['diff_date'])." jours*** pour ".$row['modele']." ".$row['marque']." | ".$row['immatriculation'];
                 sendDiscordAlert($contentDiscord);
 
                 $mail->MsgHTML($content); 
