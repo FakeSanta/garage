dbb99a3d1d9aff4e5bebb70cca0df5ea0d90fdf7
FakeSanta <aurelienfevrier08@gmail.com>
2023-06-15 07:44:57 +0000

    add input control

--- add_car.php
+++ add_car.php
@@ -8,17 +8,19 @@
     if(isset($_POST['add_car_button'])){
         if(!empty($_POST["immat"]) || !empty($_POST["marque"]) || !empty($_POST["modele"]) || !empty($_POST['radio'])){
             if(preg_match($_SESSION['pattern'], $_POST['immat'])){
-                $immat = $_POST['immat'];
-                $marque = $_POST['marque'];
-                $modele = $_POST['modele'];
+                $immat = strtoupper($_POST['immat']);
+                $marque = ucfirst($_POST['marque']);
+                $modele = ucfirst($_POST['modele']);
                 $utilitaire = $_POST['radio'];
                 $motorisation = $_POST['carburantSelect'];
                 $kilometrage = $_POST['kilometrage'];
-                $current_date = $_POST['date_ct'];
+                $current_date_ct = $_POST['date_ct'];
+                $current_date_vidange = $_POST['date_vidange'];
+                $next_date_vidange = date('Y-m-d', strtotime($current_date_vidange. '+ 1 years'));
                 if($utilitaire == 0){
-                    $next_date = date('Y-m-d', strtotime($current_date. '+ 2 years'));
+                    $next_date_ct = date('Y-m-d', strtotime($current_date_ct. '+ 2 years'));
                 }else{
-                    $next_date = date('Y-m-d', strtotime($current_date. '+ 1 years'));
+                    $next_date_ct = date('Y-m-d', strtotime($current_date_ct. '+ 1 years'));
                 }
                 if(checkCarExist($immat) === true) {
                 }
@@ -30,8 +32,11 @@
                         $selectCar = $connect->prepare("SELECT * FROM VEHICULE WHERE immatriculation = :immat");
                         $selectCar->execute(array('immat' => $immat));
                         $resultCar = $selectCar->fetch(PDO::FETCH_ASSOC);
-                        $insert_ct = $connect->prepare('INSERT INTO CT (id_vehicule, date_ct, prochaine_date_ct) VALUES (?,?,?)')->execute([$resultCar['id'], $current_date, $next_date]);
-                        $insert_histo = $connect->prepare('INSERT INTO HISTORIQUE (id_vehicule, type_operation, date_ct, commentaire) VALUES (?,?,?,"")')->execute([$resultCar['id'], $type_ope, $current_date]);
+                        $insert_ct = $connect->prepare('INSERT INTO CT (id_vehicule, date_ct, prochaine_date_ct) VALUES (?,?,?)')->execute([$resultCar['id'], $current_date_ct, $next_date_ct]);
+                        $insert_vidange = $connect->prepare('INSERT INTO VIDANGE (id_vehicule, date_vidange, prochaine_date_vidange) VALUES (?,?,?)')->execute([$resultCar['id'], $current_date_vidange, $next_date_vidange]);
+                        //$insert_ct_histo = $connect->prepare('INSERT INTO HISTORIQUE (id_vehicule, type_operation, date_ct, commentaire) VALUES (?,?,?,"")')->execute([$resultCar['id'], $type_ope, $current_date_ct]);
+                        //$insert_vidange_histo = $connect->prepare('INSERT INTO HISTORIQUE (id_vehicule, type_operation, date_vidange, commentaire) VALUES (?,?,?,"")')->execute([$resultCar['id'], $type_ope, $current_date_vidange]);
+
                         
 
                     }
@@ -42,8 +47,10 @@
                         $selectCar = $connect->prepare("SELECT * FROM VEHICULE WHERE immatriculation = :immat");
                         $selectCar->execute(array('immat' => $immat));
                         $resultCar = $selectCar->fetch(PDO::FETCH_ASSOC);
-                        $insert_ct = $connect->prepare('INSERT INTO CT (id_vehicule, date_ct, prochaine_date_ct) VALUES (?,?,?)')->execute([$resultCar['id'], $current_date, $next_date]);
-                        $insert_histo = $connect->prepare('INSERT INTO HISTORIQUE (id_vehicule, type_operation, date_ct, commentaire) VALUES (?,?,?,"")')->execute([$resultCar['id'], $type_ope, $current_date]);
+                        $insert_ct = $connect->prepare('INSERT INTO CT (id_vehicule, date_ct, prochaine_date_ct) VALUES (?,?,?)')->execute([$resultCar['id'], $current_date_ct, $next_date_ct]);
+                        $insert_vidange = $connect->prepare('INSERT INTO VIDANGE (id_vehicule, date_vidange, prochaine_date_vidange) VALUES (?,?,?)')->execute([$resultCar['id'], $current_date_vidange, $next_date_vidange]);
+
+                        //$insert_histo = $connect->prepare('INSERT INTO HISTORIQUE (id_vehicule, type_operation, date_ct, commentaire) VALUES (?,?,?,"")')->execute([$resultCar['id'], $type_ope, $current_date_ct]);
                         
                     }
                 }
